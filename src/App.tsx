d5f62210d51690d769b62ac01a223d3ce6658251
CaptainFallaway <alexander.wollmer97@gmail.com>
2025-10-15 15:15:43 +0200

    Ugly testing stuff for the fetching and types.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,67 @@
+import { useState } from "react";
+
+import {
+  type IpLookupSuccess,
+  type IpLookupFail,
+  Lookup,
+} from "@/services/ipApi";
+
+type Lookup = IpLookupSuccess | IpLookupFail | undefined;
+
+interface dumpObjectProps {
+  object: any;
+}
+
+function DumpObject({ object }: dumpObjectProps) {
+  if (object === undefined) {
+    return null;
+  }
+
+  const keyVals = Object.entries(object);
+
+  return (
+    <div className="outline p-2">
+      {keyVals.map((pair, index) => (
+        <div key={index} className="gap-2 flex">
+          <p>{pair[0]}:</p>
+          <p>{(pair[1] as any).toString()}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
 function App() {
-  return <div className="flex h-screen w-screen"></div>;
+  const [ip, setIp] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [lookup, setLookup] = useState<Lookup>(undefined);
+
+  const handleSubmit = async () => {
+    setLoading(true);
+    const lookup = await Lookup(ip);
+    console.log(lookup);
+    setLookup(lookup);
+    setIp("");
+    setLoading(false);
+  };
+
+  return (
+    <div className="flex items-center justify-center h-screen w-screen">
+      <div className="flex flex-col gap-2">
+        <input
+          type="text"
+          className="input"
+          onChange={(e) => setIp(e.target.value)}
+          value={ip}
+        />
+        {loading && <p className="font-bold">Loading...</p>}
+        <button onClick={handleSubmit} className="btn">
+          Submit
+        </button>
+        <DumpObject object={lookup} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
