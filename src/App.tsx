9024c56aeb2b3e6f315fc5f5723a6c4b432524c6
CaptainFallaway <alexander.wollmer97@gmail.com>
2025-10-16 11:20:24 +0200

    refactor: added formatting, using tabs instead of spaces

--- src/App.tsx
+++ src/App.tsx
@@ -3,59 +3,54 @@ import { useState } from "react";
 import { type IpApiResponse, Lookup } from "@/services/ipApi";
 
 interface dumpObjectProps {
-  object: any;
+	object: any;
 }
 
 function DumpObject({ object }: dumpObjectProps) {
-  if (object === undefined) {
-    return null;
-  }
+	if (object === undefined) {
+		return null;
+	}
 
-  const keyVals = Object.entries(object);
+	const keyVals = Object.entries(object);
 
-  return (
-    <div className="outline p-2">
-      {keyVals.map((pair, index) => (
-        <div key={index} className="gap-2 flex">
-          <p>{pair[0]}:</p>
-          <p>{(pair[1] as any).toString()}</p>
-        </div>
-      ))}
-    </div>
-  );
+	return (
+		<div className="py-2">
+			{keyVals.map((pair, index) => (
+				<div key={index} className="gap-2 flex">
+					<p>{pair[0]}:</p>
+					<p>{(pair[1] as any).toString()}</p>
+				</div>
+			))}
+		</div>
+	);
 }
 
 function App() {
-  const [ip, setIp] = useState("");
-  const [loading, setLoading] = useState(false);
-  const [lookup, setLookup] = useState<IpApiResponse | undefined>(undefined);
+	const [ip, setIp] = useState("");
+	const [loading, setLoading] = useState(false);
+	const [lookup, setLookup] = useState<IpApiResponse | undefined>(undefined);
 
-  const handleSubmit = async () => {
-    setLoading(true);
-    const lookup = await Lookup(ip);
-    console.log(lookup);
-    setLookup(lookup);
-    setIp("");
-    setLoading(false);
-  };
+	const handleSubmit = async () => {
+		setLoading(true);
+		const lookup = await Lookup(ip);
+		console.log(lookup);
+		setLookup(lookup);
+		setIp("");
+		setLoading(false);
+	};
 
-  return (
-    <div className="flex items-center justify-center h-screen w-screen">
-      <div className="flex flex-col gap-2">
-        <input
-          type="text"
-          className="input"
-          onChange={(e) => setIp(e.target.value)}
-          value={ip}
-        />
-        {loading && <p className="font-bold">Loading...</p>}
-        <button onClick={handleSubmit} className="btn">
-          Submit
-        </button>
-        <DumpObject object={lookup} />
-      </div>
-    </div>
-  );
+	return (
+		<div className="flex items-center justify-center h-screen w-screen">
+			<div className="flex flex-col gap-2">
+				<input type="text" className="input" onChange={(e) => setIp(e.target.value)} value={ip} />
+				{loading && <p className="font-bold">Loading...</p>}
+				<button onClick={handleSubmit} className="btn">
+					Submit
+				</button>
+				<DumpObject object={lookup} />
+			</div>
+		</div>
+	);
 }
 
 export default App;
