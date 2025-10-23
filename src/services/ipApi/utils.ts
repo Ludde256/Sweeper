fb51909519e0a652568238171752382340dc2330
CaptainFallaway <alexander.wollmer97@gmail.com>
2025-10-23 14:26:23 +0200

    fix: changed property name

--- src/services/ipApi/utils.ts
+++ src/services/ipApi/utils.ts
@@ -3,6 +3,6 @@ import type { IpApiResponse, IpLookupSuccess } from "./types";
 // This is a type guard function and it tells the TypeScript compiler that
 // if this function returns true, then the data is of type IpLookupSuccess
 // Useful in conditional checks to narrow down types.
-export function IsLookupSuccess(data: IpApiResponse): data is IpLookupSuccess {
-	return data.status === "success";
+export function IsLookupSuccess(response: IpApiResponse): response is IpLookupSuccess {
+	return response.status === "success";
 }
