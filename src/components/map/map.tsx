4101c552d762949688ab8810b88d95f0d33b0709
CaptainFallaway <alexander.wollmer97@gmail.com>
2025-11-03 09:53:25 +0100

    feat: refactor Map component and update leaflet-bar styling for improved marker handling and UI consistency

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -1,26 +1,46 @@
+import L from "leaflet";
 import { useLookups } from "@/hooks/lookups";
 import { useMemo } from "react";
-import L, { type LatLngTuple } from "leaflet";
 import { MapContainer, Marker, TileLayer, Tooltip } from "react-leaflet";
 
 import { Recenter } from "./recenter";
 
-export const DEFAULT_POS = L.latLng(51.505, -0.09);
+export const DEFAULT_POS = [51.505, -0.09] as L.LatLngTuple; // London
 export const DEFAULT_ZOOM = 10;
+
+type IpMarker = {
+	latLng: L.LatLngTuple;
+	Ip: string;
+};
 
 export function Map() {
 	const { lookups } = useLookups();
 
-	const positions = useMemo(() => lookups.map((l) => [l.lat, l.lon] as LatLngTuple), [lookups]);
+	const markers: IpMarker[] = useMemo(
+		() =>
+			lookups.map((l) => {
+				return {
+					latLng: [l.lat, l.lon] as L.LatLngTuple,
+					Ip: l.query,
+				};
+			}),
+		[lookups]
+	);
+
+	const positions = useMemo(() => markers.map((marker) => marker.latLng), [markers]);
 
 	return (
-		<MapContainer center={DEFAULT_POS} zoom={DEFAULT_ZOOM} className="rounded-md w-full h-[20vh] md:h-full box">
+		<MapContainer
+			center={DEFAULT_POS}
+			zoom={DEFAULT_ZOOM}
+			className="z-0 rounded-md w-full h-[30vh] lg:flex-1 lg:h-full box"
+		>
 			<TileLayer
 				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 			/>
-			{positions.map((pos, index) => (
-				<Marker key={`${lookups[index].query}-${pos[0]}-${pos[1]}`} position={pos}>
+			{markers.map((marker, index) => (
+				<Marker key={`${marker.Ip}-${marker.latLng[0]}-${marker.latLng[1]}`} position={marker.latLng}>
 					<Tooltip>
 						<span>{lookups[index].query}</span>
 					</Tooltip>
